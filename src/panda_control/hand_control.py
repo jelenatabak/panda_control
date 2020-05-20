cdb2fd36a427750092df8362a03ad8e868adf0e9
Jelena <jelena.tabak@gmail.com>
2020-05-20 23:44:31 +0200

    Update hand_control

--- src/panda_control/hand_control.py
+++ src/panda_control/hand_control.py
@@ -6,11 +6,11 @@ import math
 import tf
 import moveit_commander
 from geometry_msgs.msg import WrenchStamped
-from geometry_msgs.msg import Pose, Point, Quaternion
 
 class ForceSensor(object):
     def __init__(self, topic):
         self.topic = topic
+        self.listener = tf.TransformListener()
 
     def sum_of_forces(self):
         message = rospy.wait_for_message(self.topic, WrenchStamped)
@@ -18,6 +18,7 @@ class ForceSensor(object):
         y = abs(message.wrench.force.y)
         z = abs(message.wrench.force.z)
         return x+y+z
+
 
 class HandControl(object):
     def __init__(self):
@@ -42,22 +43,38 @@ class HandControl(object):
         self.move_group_hand.go(joint_goal, wait=True)
         self.move_group_hand.stop()
 
-	def go_to_pose_goal(self):
-		self.move_group_arm.set_pose_target(self.pose_goal)
+    def update_arm_pose(self, dx):
+        pose_goal = self.move_group_arm.get_current_pose().pose
+        angles = tf.transformations.euler_from_quaternion([pose_goal.orientation.x,
+        pose_goal.orientation.y, pose_goal.orientation.z, pose_goal.orientation.w])
+        theta = angles[2]
 
-		plan = self.move_group_arm.plan()
-		if plan.joint_trajectory.points:
-				move_success = self.move_group_arm.execute(plan)
-		else:
-		  rospy.logerr("Trajectory is empty. Planning was unsuccessful.")
+        direction = 0
+        if self.right_sensor.sum_of_forces() > self.left_sensor.sum_of_forces():
+            direction = -1
+        else:
+            direction = 1
 
-		self.move_group_arm.stop()
-		self.move_group_arm.clear_pose_targets()
+        pose_goal.position.x += direction*dx*math.sin(theta)
+        pose_goal.position.y += direction*dx*math.cos(theta)
+
+        self.move_group_arm.set_pose_target(pose_goal)
+
+        plan = self.move_group_arm.plan()
+        if plan.joint_trajectory.points:
+            move_success = self.move_group_arm.execute(plan)
+        else:
+            rospy.logerr("Trajectory is empty. Planning was unsuccessful.")
+
+        self.move_group_arm.stop()
+        self.move_group_arm.clear_pose_targets()
 
     def impendance_control(self):
         while (self.left_sensor.sum_of_forces()+self.right_sensor.sum_of_forces()) < 0.01:
-            print(self.left_sensor.sum_of_forces()+self.right_sensor.sum_of_forces())
             self.close_gripper(0.001);
 
-        # ako je jedan u 0 a drugi nije, pomakni po osi hvataljke - postavi self.pose_goal i pozovi go_to_pose_goal()
-        # impedantno
+        while(abs(self.left_sensor.sum_of_forces()-self.right_sensor.sum_of_forces())) > 0.005:
+            rospy.loginfo("Moving the arm")
+            self.update_arm_pose(0.005)
+            rospy.loginfo(self.left_sensor.sum_of_forces())
+            rospy.loginfo(self.right_sensor.sum_of_forces())
