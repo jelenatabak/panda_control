4f8ce7c19bdf333291837bf58d1b56d2b4d87feb
Jelena <jelena.tabak@gmail.com>
2020-06-13 12:20:17 +0200

    Update impedance control

--- src/panda_control/hand_control.py
+++ src/panda_control/hand_control.py
@@ -43,12 +43,27 @@ class HandControl(object):
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 rospy.logerr("Waiting for the transformation to become available..")
 
-        pose = PoseStamped()
-        pose.header.frame_id = "panda_leftfinger"
-        pose.header.stamp = rospy.Time.now()
-        pose.pose = Pose(position=Point(x=position[0],y=position[1],z=position[2]),
+        self.left_finger_pose = PoseStamped()
+        self.left_finger_pose.header.frame_id = "panda_leftfinger"
+        self.left_finger_pose.header.stamp = rospy.Time.now()
+        self.left_finger_pose.pose = Pose(position=Point(x=position[0],y=position[1],z=position[2]),
                             orientation=Quaternion(x=quaternion[0],y=quaternion[1],z=quaternion[2],w=quaternion[3]))
-        self.pub_pose.publish(pose)
+        self.pub_pose.publish(self.left_finger_pose)
+
+        while not rospy.is_shutdown():
+            try:
+                position,quaternion = self.listener.lookupTransform('/world', '/panda_rightfinger', rospy.Time(0))
+                break
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                rospy.logerr("Waiting for the transformation to become available..")
+
+        self.right_finger_pose = PoseStamped()
+        self.right_finger_pose.header.frame_id = "panda_rightfinger"
+        self.right_finger_pose.header.stamp = rospy.Time.now()
+        self.right_finger_pose.pose = Pose(position=Point(x=position[0],y=position[1],z=position[2]),
+                            orientation=Quaternion(x=quaternion[0],y=quaternion[1],z=quaternion[2],w=quaternion[3]))
+        #self.pub_pose.publish(self.left_finger_pose)
+
 
     def open_gripper(self):
         joint_goal = self.move_group_hand.get_current_joint_values()
@@ -57,24 +72,27 @@ class HandControl(object):
         self.move_group_hand.go(joint_goal, wait=True)
         self.move_group_hand.stop()
 
-    def close_gripper(self, dx):
+    def update_width (self, dx):
+        rospy.loginfo("Changing gripper width for: %f", dx)
         joint_goal = self.move_group_hand.get_current_joint_values()
-        joint_goal[0] -= dx
-        joint_goal[1] -= dx
+
+        rospy.loginfo(joint_goal)
+        if (joint_goal[0] > 0.039 or joint_goal[1] > 0.039) and dx > 0:
+            self.open_gripper()
+            rospy.loginfo("Gripper width reached maximum!")
+            return 0
+
+        joint_goal[0] += dx
+        joint_goal[1] += dx
         self.move_group_hand.go(joint_goal, wait=True)
         self.move_group_hand.stop()
+        return 1
 
-    def update_arm_pose(self, dx):
+    def update_arm_pose(self, dx, direction):
         pose_goal = self.move_group_arm.get_current_pose().pose
         angles = tf.transformations.euler_from_quaternion([pose_goal.orientation.x,
         pose_goal.orientation.y, pose_goal.orientation.z, pose_goal.orientation.w])
         theta = angles[2]
-
-        direction = 0
-        if self.right_sensor.sum_of_forces() > self.left_sensor.sum_of_forces():
-            direction = -1
-        else:
-            direction = 1
 
         pose_goal.position.x += direction*dx*math.sin(theta)
         pose_goal.position.y += direction*dx*math.cos(theta)
@@ -90,22 +108,77 @@ class HandControl(object):
         self.move_group_arm.stop()
         self.move_group_arm.clear_pose_targets()
 
+    def force_determine_direction(self):
+        if self.right_sensor.sum_of_forces() > self.left_sensor.sum_of_forces():
+            return -1
+        else:
+            return 1
+
+    def pose_callback(self, data):
+        if self.equal_to_current_pose(data):
+            self.starter(False)
+            return
+
+        direction = self.pose_determine_direction(data, self.left_finger_pose, self.right_finger_pose)
+        if not self.update_width(direction*0.001):
+            self.starter(False)
+        else:
+            self.publish_pose()
+            rospy.sleep(3)
+
+    def equal_to_current_pose(self, data):
+        xc = self.left_finger_pose.pose.position.x
+        yc = self.left_finger_pose.pose.position.y
+        zc = self.left_finger_pose.pose.position.z
+
+        xr = data.position.x
+        yr = data.position.y
+        zr = data.position.z
+
+        if math.sqrt((xc-xr)**2 + (yc-yr)**2 + (zc-zr)**2) < 0.0001:
+            return True
+        else:
+            return False
+
+    def pose_determine_direction(self, ref_pose, finger_pose, other_finger_pose):
+        xr = ref_pose.position.x
+        yr = ref_pose.position.y
+        xc = finger_pose.pose.position.x
+        yc = finger_pose.pose.position.y
+        xo = other_finger_pose.pose.position.x
+        yo = other_finger_pose.pose.position.y
+
+        ref_width = math.sqrt((xo-xr)**2 + (yo-yr)**2)
+        current_width =  math.sqrt((xo-xc)**2 + (yo-yc)**2)
+
+        if ref_width < current_width:
+            return -1
+        else:
+            return 1
+
     def impendance_control(self):
-        while (self.left_sensor.sum_of_forces()+self.right_sensor.sum_of_forces()) < 0.01:
-            self.close_gripper(0.001);
+        while (self.left_sensor.sum_of_forces()+self.right_sensor.sum_of_forces()) < 0.1:
+            self.update_width(-0.001);
 
         while(abs(self.left_sensor.sum_of_forces()-self.right_sensor.sum_of_forces())) > 0.05:
             rospy.loginfo("Moving the arm")
-            self.update_arm_pose(0.005)
+            direction = self.force_determine_direction()
+            self.update_arm_pose(0.005, direction)
             rospy.loginfo(self.left_sensor.sum_of_forces())
             rospy.loginfo(self.right_sensor.sum_of_forces())
 
         while not self.pub_pose.get_num_connections():
-            rospy.logerr("Waiting for connections, run impedance_control!")
             rospy.sleep(1)
 
         self.publish_pose()
 
         rospy.wait_for_service('/impedance_control/start')
-        starter = rospy.ServiceProxy('/impedance_control/start', SetBool)
-        starter(True)
+        self.starter = rospy.ServiceProxy('/impedance_control/start', SetBool)
+        self.starter(True)
+
+        rospy.Subscriber('/impedance_control/pose_output', Pose, self.pose_callback)
+        rospy.spin()
+
+        # pomaknut ruku nakon zatvaranja hvataljke tako da na desnom senzoru nema promjena
+        # kreirat impedance_control i za desni senzor (iz launcha?)
+        # za desni raditi pomake ruke, a hvataljku podesavati tako da nema promjene na lijevom
